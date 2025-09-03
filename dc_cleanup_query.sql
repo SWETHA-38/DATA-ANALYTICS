eb873832c9bb2a6ce1cd7ca73800d3356d3b10c8
SWETHA-38 <113578437+SWETHA-38@users.noreply.github.com>
2025-09-03 16:17:05 +0530

    Add files via upload

---
+++ dc_cleanup_query.sql
@@ -0,0 +1,355 @@
+
+  --select * from answertemplate_category
+
+  delete from answertemplate_category
+
+
+  --select * from answertemplate_subCategory
+
+
+  delete from answertemplate_subCategory
+
+  
+  --select * from authentication_tokendictvalue
+
+  delete from authentication_tokendictvalue
+
+  --select * from authentication_userloginstatus
+
+  delete from authentication_userloginstatus
+
+  --select * from blogger
+
+  delete from blogger
+
+  --select * from businessunit
+
+  businessunit --10159 NFBU
+  
+  delete from businessunit where buId!=10159
+  
+--update businessunit set businessgroup_fk=10070 where buId=10159
+
+  --select * from businessunit_group
+
+  delete from businessunit_group where bugroupId!=10070
+		
+-- businessunit_group   10070 -RC-ADM
+
+  --select * from category
+
+  delete from category where id>6
+
+  --select * from SurveyFeedback
+
+  delete from SurveyFeedback
+
+  --select * from design_product
+
+  delete from design_product
+
+  --select * from design_item
+
+  delete from design_item
+
+  --select * from design_template
+
+
+  delete from design_template
+
+  --select * from chat
+
+  delete from chat
+
+  --select * from chatmessage
+
+  delete from chatmessage
+
+  --select * from comment
+
+  delete from comment
+
+  --select * from commentmessage
+
+  delete from commentmessage
+
+
+  --select * from Customertable
+
+  delete from Customertable
+
+  --select * from datadictcategory
+
+  --delete from datadictcategory
+
+  --select * from dd_item
+
+  delete from dd_item
+
+  --select * from dd_property
+
+  delete from dd_property
+
+  --select * from DemoMastertext
+
+  delete from DemoMastertext
+
+  --select * from design_approval
+
+  delete from design_approval
+
+  --select * from design_changeslog
+
+  delete from design_changeslog
+
+  --select * from designtemplatefile
+
+  delete from designtemplatefile
+
+  --select * from devicedata
+
+  delete from devicedata
+
+  --select * from documentbuild
+
+  delete from documentbuild
+
+  --select * from EventLog
+
+  delete from EventLog
+
+  --select * from float_content
+  
+  delete from float_content
+
+  --select * from float_content
+
+  --select * from forget_useraccount
+
+  delete from forget_useraccount
+
+  --select * from forgetpassword_person
+
+  delete from forgetpassword_person
+  
+  --select * from forgetpassword_referencemodel
+  
+  delete from forgetpassword_referencemodel
+
+  --select * from form_approval
+
+  delete from form_approval
+
+  --select * from form_assigned
+
+  delete from form_assigned
+
+  
+  --select * from webservice_audit_dailytrans
+
+  delete from webservice_audit_dailytrans
+  
+  --select * from webservice_audit
+
+  delete from webservice_audit
+
+  --select * from webservice_archive_dailytrans
+
+  delete from webservice_archive_dailytrans
+
+
+  --select * from form_product 
+
+  delete from form_product
+
+  --select * from form_item
+
+  --select * from form_template
+  
+  --select * from form_category
+
+
+  delete from form_category where formcategoryId!=1
+
+  
+  --select * from ImageUpload_filemodel
+
+  delete from ImageUpload_filemodel
+
+  --select * from ImageUpload_referencesmodel
+
+  delete from ImageUpload_referencesmodel
+
+  --select * from inbox
+
+  delete from inbox
+
+  --select * from masterLibrary_filemodel
+
+  delete from masterLibrary_filemodel
+
+  --select * from masterLibrary_uploadingmodel
+
+  delete from masterLibrary_uploadingmodel
+  
+  --select * from notificationsetting
+
+  delete from notificationsetting
+
+  --select * from OptionItem
+
+  delete from OptionItem
+
+  --select * from OptionCategory
+
+  delete from OptionCategory
+  
+  --select * from OTPValidation
+
+  delete from OTPValidation
+
+  --select * from pdftransaction
+
+  delete from pdftransaction
+
+  --select * from PdfUpload_referencespdfmodel
+
+  delete from PdfUpload_referencespdfmodel
+
+  --select * from sampledate
+
+  delete from sampledate
+
+  --select * from secondapp_first
+
+  delete from secondapp_first
+  
+  --select * from SecQuesFailure
+
+  delete from SecQuesFailure
+
+  --select * from share
+
+   delete from share
+
+   --select * from SSOAuditLog
+
+   delete from SSOAuditLog
+
+   --select * from SsoSyncErrorLog
+
+   delete from SsoSyncErrorLog
+
+   
+   --select * from SurveyDictionaryProperty
+
+   delete from SurveyDictionaryProperty
+  
+  --select * from SurveyDictionaryItem
+
+   delete from SurveyDictionaryItem
+
+  
+   --select * from SurveyDictionaryCategory
+
+   delete from SurveyDictionaryCategory
+
+   --select * from SurveyDictionaryMappedList
+   delete from SurveyDictionaryMappedList
+
+   --select * from SurveyFeedback
+
+   delete from SurveyFeedback
+
+   --select * from surveyfeedbackresults
+
+   delete from surveyfeedbackresults
+
+   --select * from template
+
+   delete from template
+
+   --select * from templatefile
+
+   delete from templatefile
+
+   --select * from templatemaster
+
+   delete from templatemaster
+
+   --select * from toolkit_item
+
+   --delete from toolkit_item
+   --select * from toolkit_category
+
+--delete from toolkit_category
+
+   --select * from UploadImage_filelist
+
+   --delete from UploadImage_filelist
+
+   --select * from UploadImage_references
+
+   --delete from UploadImage_references
+
+   --select * from user_account
+
+   --select * from user_assignedrole
+
+   delete from user_assignedrole
+
+   --select * from user_audit
+
+   delete from user_audit
+
+   --select * from user_role
+   
+   --select * from user_rolegroup
+
+   --select * from UserLoginStatus
+
+   --select * from userroletype
+
+   delete from userroletype
+
+   --select * from userscreen
+
+   --select * from UserSecCodeAudit
+
+   delete from UserSecCodeAudit
+
+   --select * from userTempTokens
+
+   delete from userTempTokens
+
+   --select * from variables
+
+   --select * from webservice_archieve
+
+   --select * from webservice_archive_dailytrans
+
+   --select * from webservice_audit
+
+   --select * from webservice_audit_dailytrans
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
